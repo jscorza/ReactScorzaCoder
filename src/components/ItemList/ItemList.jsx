461edf289fb0b53df63cbeab9ad71f5544cab834
jscorza <jscorza@itba.edu.ar>
2022-11-10 20:28:50 -0300

    estoy en la b

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Item from '../Item/Item'
+<link rel="stylesheet" href="./ItemList.scss" />
 
 //Se encarga de hacer el map con los productos
 const ItemList = ({products}) => {
   return (
-    products.map(product => {
+    <div className='lista'>
+    {products.map(product => {
       console.log(product)
         return <Item key={product.id}  product={product}/>
-        
-    })
-    )
+    
+    })}
+  </div>
+  )
+
 }
 
 export default ItemList
